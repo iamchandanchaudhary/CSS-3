d2e6a474473c865a73876305ce53074b8becaea5
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2024-12-27 11:16:27 +0530

    Improvement 43

--- Classroom/Projects/Registration Form/app.js
+++ Classroom/Projects/Registration Form/app.js
@@ -1,17 +1,12 @@
-let btn = document.querySelector("#sub-btn");
+let btn = document.querySelector(".sub-btn");
 let inpArea = document.querySelector(".input-area");
-
-// let newPop = document.createElement("p");
-// newPop.innerText = "Form was Submited... 👍";
-
-// let mainBox = document.querySelector(".main-box");
+const popup = document.querySelector(".popup");
+let okbtn = document.querySelector(".okbtn");
 
 btn.addEventListener("click", () => {
-
-    // alert("Form was Registerd.");
-    // inpArea.remove();
-    // btn.remove();
-
     popup.classList.add("open-popup");
-    // mainBox.appendChild(newPop);
+})
+
+okbtn.addEventListener("click", () => {
+    popup.classList.remove("open-popup");
 })
